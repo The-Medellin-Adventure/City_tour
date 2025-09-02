7436d693023e96d79aa0ddda2ddc78558d92770a
The-Medellin-Adventure <dcaame@gmail.com>
2025-09-02 18:44:16 -0500

    Update package.js

--- api/package.js
+++ api/package.js
@@ -1,10 +1,11 @@
 {
-"name": "tour360-seguro",
-"version": "1.0.0",
-"private": true,
-"type": "module",
-"dependencies": {
-"@supabase/supabase-js": "^2.45.0",
-"nanoid": "^5.0.7"
-}
+  "name": "tour360-seguro",
+  "version": "1.0.0",
+  "private": true,
+  "type": "module",
+  "dependencies": {
+    "@supabase/supabase-js": "^2.45.0",
+    "nanoid": "^5.0.7",
+    "@sendgrid/mail": "^8.1.0"
+  }
 }
