f257899716965f6c84bd74600056e60545dcd753
The-Medellin-Adventure <dcaame@gmail.com>
2025-09-03 10:34:49 -0500

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>CityTour360 API</title>
+  </head>
+  <body>
+    <h1>CityTour360 está online 🚀</h1>
+    <p>Usa las funciones en <code>/api</code> para acceder.</p>
+  </body>
+</html>
