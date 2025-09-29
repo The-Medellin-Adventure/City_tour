8f0ca4d2acd894f964a890fd74c3da205d8b256a
The-Medellin-Adventure <dcaame@gmail.com>
2025-09-29 16:27:27 -0500

    Update signed-url.js

--- api/signed-url.js
+++ api/signed-url.js
@@ -1,5 +1,5 @@
 ```js
-import { supabaseAdmin } from '../lib/_supabaseClient.js';
+import { supabaseAdmin } from './_supabaseClient.js';
 
 export default async function handler(req, res) {
   try {
